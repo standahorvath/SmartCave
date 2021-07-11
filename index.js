803053ca75fb54b741e13dcea37786bf26418958
standahorvath <standa@tonakodi.cz>
2021-07-11 21:49:56 +0200

    Add express module

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const express = require('express')
+const app = express(80)
 
 const mongo_driver = require('./modules/mongo');
 const udp_driver = require('./modules/udp_driver');
 const udp_router = require('./app/router');
 
+
+app.get('/', (req, res) => {
+  res.sendFile( __dirname + "/static/" + "index.html" );
+})
+
+var server = app.listen(80, function () {
+  var host = server.address().address
+  var port = server.address().port
+  
+  console.log("Website http://%s:%s", host, port)
+})
 
 mongo_driver.init(
   udp_driver.init(
